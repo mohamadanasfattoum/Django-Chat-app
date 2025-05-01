8b753917e20d5dfb2ee25838090544b8b97364bc
mohamadanasfattoum <samaanas30@gmail.com>
2025-05-01 23:43:04 +0200

    add edit page and funktion

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
@@ -25,3 +26,5 @@ urlpatterns = [
     path("profile/", include("users.urls")),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import profile
+from .views import profile, edit_profile
 
 urlpatterns = [
     path('', profile, name='profile'),
+    path('edit/', edit_profile, name='edit_profile'),
 ]
