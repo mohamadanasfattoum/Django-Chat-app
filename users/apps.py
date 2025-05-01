17af34c206f66c8a63d885c8f1cd226bbf39b9f6
mohamadanasfattoum <samaanas30@gmail.com>
2025-05-01 19:02:59 +0200

    created signals.py to create function to create profile if user created

--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
+
+
+    def ready(self):
+        import users.signals
+        # This method is called when the app is ready
+        # You can use it to perform any initialization tasks
---
+++ users/signals.py
@@ -0,0 +1,17 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def user_postsave(sender, instance, created, **kwargs):
+    user = instance
+    # Check if the user is created
+    # and if the profile does not exist
+    # If the user is created, create a profile
+    # If the user is not created, do nothing
+    if created:
+        Profile.objects.create(
+            user=user,
+                               )
