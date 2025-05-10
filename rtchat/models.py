4219720da15dfbadb517ece13ce209bd461f8ffb
mohamadanasfattoum <samaanas30@gmail.com>
2025-05-10 18:01:01 +0200

    add is_private and members fields to ChatGroup model; set default group_name to short UUID & installed channels-redis and shortuuid

--- rtchat/models.py
+++ rtchat/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+import shortuuid
 
 # Create your models here.
 class ChatGroup(models.Model):
-    group_name = models.CharField(max_length=128, unique=True) # Unique group name
+    group_name = models.CharField(max_length=128, unique=True, default=shortuuid.uuid) # Unique group name, default to a short UUID
     users_online = models.ManyToManyField(User, related_name='online_in_groups',blank=True) # Many-to-many relationship with User model
-
-
+    members = models.ManyToManyField(User, related_name='chat_groups',blank=True) # Many-to-many relationship with User model
+    is_private = models.BooleanField(default=False) # Boolean field to indicate if the group is private
+    
     def __str__(self):
         return self.group_name
     
